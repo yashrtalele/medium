5020e33dba64239324ab621c5942c6f15ecfef90
Yash Talele <yashrtalele@gmail.com>
2024-06-25 19:36:38 +0530

    Created routes for user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,15 @@
 import { Hono } from "hono";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
 
-const app = new Hono();
+export const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup route");
-});
-
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  const id = c.req.param("id");
-  console.log(id);
-  return c.text("get blog route");
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("signin route");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("signin route");
-});
-
-app.get("/api/v1/blog/all", (c) => {
-  return c.text("get all blogs");
-});
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app;
