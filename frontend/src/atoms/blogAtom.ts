a41edd0b4d10ee8d558e1898a68202cae48eb9cc
Yash Talele <yashrtalele@gmail.com>
2024-06-26 18:55:56 +0530

    Changed from static url to importing BACKEND_URL from config

--- frontend/src/atoms/blogAtom.ts
+++ frontend/src/atoms/blogAtom.ts
@@ -1,4 +1,5 @@
 import { atom, selectorFamily } from "recoil";
+import { BACKEND_URL } from "../config";
 import axios from "axios";
 
 export const selectedBlogIdAtom = atom({
@@ -11,14 +12,11 @@ export const blogDetailSelector = selectorFamily({
   get: (blogId) => async () => {
     if (!blogId) return null;
     try {
-      const response = await axios.get(
-        `https://backend.yash-talele97.workers.dev/api/v1/blog/${blogId}`,
-        {
-          headers: {
-            Authorization: localStorage.getItem("token"),
-          },
+      const response = await axios.get(`${BACKEND_URL}/api/v1/blog/${blogId}`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
         },
-      );
+      });
       return response.data;
     } catch (error) {
       console.error("Error fetching blog details: ", error);
