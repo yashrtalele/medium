194526202fdd63410a97e9a591f9a40f73a4b76b
Yash Talele <yashrtalele@gmail.com>
2024-06-26 16:57:45 +0530

    Added Signup functionality

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,129 @@
+import { ChangeEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signupInput } from "@yashrtalele/medium-common";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const navigate = useNavigate();
+  const [postInputs, setPostInputs] = useState<signupInput>({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  async function sendRequest() {
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
+        postInputs,
+      );
+      const jwt = response.data;
+      localStorage.setItem("token", jwt);
+      navigate("/blogs");
+    } catch (e) {
+      alert("Error while signing up");
+    }
+  }
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-500">
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Sign up" : "Sign in"}
+              </Link>
+            </div>
+          </div>
+          <div className="pt-8">
+            {type === "signup"
+              ? (
+                <LabeledInput
+                  label="Name"
+                  placeholder="John Doe"
+                  id="name"
+                  onChange={(e) => {
+                    setPostInputs({
+                      ...postInputs,
+                      name: e.target.value,
+                    });
+                  }}
+                />
+              )
+              : null}
+            <LabeledInput
+              label="Email"
+              placeholder="john.doe@email.com"
+              id="email"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  email: e.target.value,
+                });
+              }}
+            />
+            <LabeledInput
+              label="Password"
+              type={"password"}
+              placeholder="123456"
+              id="password"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <button
+              onClick={sendRequest}
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign Up" : "Sign In"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface LabeledInputType {
+  label: string;
+  placeholder: string;
+  id: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+
+function LabeledInput({
+  label,
+  placeholder,
+  id,
+  onChange,
+  type,
+}: LabeledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm text-black font-semibold pt-4">
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type || "text"}
+        id={id}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
